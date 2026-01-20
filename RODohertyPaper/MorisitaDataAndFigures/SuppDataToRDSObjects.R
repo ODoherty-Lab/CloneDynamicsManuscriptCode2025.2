896390d6736feec104a7a76c1be5978cf46e0d53
SamWeissman417 <samsweissman@gmail.com>
2026-01-20 13:20:23 -0500

    Update SuppDataToRDSObjects.R
    changing some comments to update supp data numbers

--- RODohertyPaper/MorisitaDataAndFigures/SuppDataToRDSObjects.R
+++ RODohertyPaper/MorisitaDataAndFigures/SuppDataToRDSObjects.R
@@ -1,6 +1,6 @@
 base = here::here()
-pathToSuppData1 = paste0(base, "/MorisitaDataAndFigures/SuppData/Supplementary Data 1-ClonesTables.xlsx")
-pathToSuppData2 = paste0(base, "/MorisitaDataAndFigures/SuppData/Supplementary Data 2-TCRsSequenced.xlsx")
+pathToSuppData1 = paste0(base, "/MorisitaDataAndFigures/SuppData/Supplementary Data 1-ClonesTables.xlsx") # make sure copied this file into directory from paper online.
+pathToSuppData2 = paste0(base, "/MorisitaDataAndFigures/SuppData/Supplementary Data 2-TCRsSequenced.xlsx") # make sure copied this file into directory from paper online.
 pathToTCRRDS = paste0(base, "/MorisitaDataAndFigures/RDSObjects/TCRSimpleObjects.rds")
 outFolderPath = paste0(base, "/MorisitaDataAndFigures/RDSObjects")
 
@@ -10,10 +10,10 @@ source(paste0(base, "/MorisitaDataAndFigures/HelpingFunctions/TCRHelpFunctions.R
 
 # these all should take a few min, the TCR can take few min per person because reading excel but is verbose about it, not much longer though.
 
-# saves supp data 4 to RDS for each sheet as a list, and to list of TCR simple objects.
+# saves supp data 2 to RDS for each sheet as a list, and to list of TCR simple objects.
 RDSFromSuppData2(pathToSuppData2, outFolderPath)
 
-# saves supp data 5 to RDS for list of provirus simple objects, and for CT2 saves another RDS simple object for CT2 if timepoint repeats are counted separately
+# saves supp data 1 to RDS for list of provirus simple objects, and for CT2 saves another RDS simple object for CT2 if timepoint repeats are counted separately
 RDSFromSuppData1(pathToSuppData1, outFolderPath)
 
 
